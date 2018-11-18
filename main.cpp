c93985125a0aa47b2816f893b7f1fcdbc3c2c2c8
Vitaly Petrov <v31337@gmail.com>
2018-11-18 22:43:03 +0300

    Replace operator[] to operator() for matrix for support variadic templates

--- main.cpp
+++ main.cpp
@@ -6,8 +6,6 @@
 //#define ENABLE_2D_MATRIX
 //#define ENABLE_3D_MATRIX
 
-using Index = std::vector<int>;
-
 template<typename T, T DefVal = -1, size_t Dimension = 2>
 class Matrix
 {
@@ -24,13 +22,29 @@ public:
     struct Proxy
     {
         ContainerType *container;
-        Index index;
-        Proxy(ContainerType *a, Index index) : container(a), index(index) {}
+        std::vector<int> index;
+
+        void fillIndex()
+        {
+            //std::cout << std::endl;
+        }
+
+        template<typename Tv, typename... Args>
+        void fillIndex(Tv tv, Args&& ... args)
+        {
+            index.push_back(tv);
+            fillIndex(args...);
+        }
+
+        template <typename ... Args >
+        Proxy(ContainerType *a, Args&& ... args) : container(a)
+        {
+            fillIndex(args...);
+            assert(this->index.size() == Dimension);
+        }
 
         operator T()
         {
-            assert(index.size() == Dimension);
-
             if (container->m_data.find(index) != container->m_data.end())
                 return container->m_data[index];
             else
@@ -39,23 +53,29 @@ public:
 
         T& operator=(const T& other)
         {
-            assert(index.size() == Dimension);
-
             //do not store default values
             if (other == DefVal)
+            {
+                container->m_data.erase(index);
                 return const_cast<T&>(other);
+            }
 
             return container->m_data[index] = other;
         }
     };
 
-    auto operator[](Index index)
+    //operator[] does not support multi-arguments =(
+    //More info: https://stdcpp.ru/proposals/ebfbd6dc-7848-4864-8b5c-3c18cdc91b61
+    //workaround with operator()
+    template <typename ... Args >
+    auto operator()(Args&& ... args)
     {
-        return Proxy<Matrix<T, DefVal, Dimension> >(this, index);
+        //std::cout << "sizeof1 " << sizeof...(args) << std::endl;
+        return Proxy<Matrix<T, DefVal, Dimension> >(this, std::forward <Args>(args) ...);
     }
 
 private:
-    std::map<Index, T> m_data;
+    std::map<std::vector<int>, T> m_data;
 };
 
 int main(int, char *[])
@@ -64,12 +84,12 @@ int main(int, char *[])
     Matrix<int, -1, 2> matrix2d;
     assert(matrix2d.size() == 0); // все ячейки свободны
 
-    auto a2d = matrix2d[Index({0,0})];
+    auto a2d = matrix2d(2, 4);
     assert(a2d == -1);
     assert(matrix2d.size() == 0);
 
-    matrix2d[Index({100,100})] = 314;
-    assert(matrix2d[Index({100,100})] == 314);
+    matrix2d(100,100) = 314;
+    assert((matrix2d(100,100)) == 314);
     assert(matrix2d.size() == 1);
 
     // выведется одна строка
@@ -86,12 +106,12 @@ int main(int, char *[])
     Matrix<int, -1, 3> matrix3d;
     assert(matrix3d.size() == 0); // все ячейки свободны
 
-    auto a3d = matrix3d[Index({0,0,0})];
+    auto a3d = matrix3d(0,0,0);
     assert(a3d == -1);
     assert(matrix3d.size() == 0);
 
-    matrix3d[Index({100, 100, 100})] = 314;
-    assert(matrix3d[Index({100, 100, 100})] == 314);
+    matrix3d(100, 100, 100) = 314;
+    assert(matrix3d(100, 100, 100) == 314);
     assert(matrix3d.size() == 1);
 
     // выведется одна строка
@@ -108,8 +128,8 @@ int main(int, char *[])
     Matrix<int, 0, 2> matrix;
     for(int i = 0, j = 0; i <= 9; ++i, ++j)
     {
-        matrix[Index({i, i})] = i;
-        matrix[Index({9 - i, i})] = 9 - i;
+        matrix(i, i) = i;
+        matrix(9 - i, i) = 9 - i;
     }
 
     //print matrix fragment
@@ -117,7 +137,7 @@ int main(int, char *[])
     {
         for(int j = 1; j <= 8; ++j)
         {
-            std::cout << matrix[Index({i, j})] << (j < 8 ? " " : "");
+            std::cout << matrix(i, j) << (j < 8 ? " " : "");
         }
         std::cout << std::endl;
     }
@@ -132,8 +152,16 @@ int main(int, char *[])
         std::cout << c.second << std::endl;
     }
 
-    ((matrix[Index({100, 100})] = 314) = 0) = 217;
-    //std::cout << matrix[Index({100, 100})] << std::endl;
+    ((matrix(100, 100) = 314) = 0) = 217;
+    assert(matrix(100, 100) == 217);
+    assert(matrix.size() == 19);
+    //std::cout << matrix(100, 100) << std::endl; //print 217
+
+    //check removing of element
+    matrix(100, 100) = 314;
+    assert(matrix.size() == 19);
+    matrix(100, 100) = 0;
+    assert(matrix.size() == 18);
 
     return 0;
 }
